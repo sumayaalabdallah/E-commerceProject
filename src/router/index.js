ce84a02a71288767ac57da832d7eefeda49b2d15
sumayaalabdallah <sumayaalabdallah@gmail.com>
2024-04-02 20:07:33 +0300

    up33

--- src/router/index.js
+++ src/router/index.js
@@ -46,15 +46,6 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/LikesPage.vue'),
       },
-      {
-        path: '/login',
-        name: 'login',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/loginPage.vue'),
-      },
-
     ],
   },
 ]
